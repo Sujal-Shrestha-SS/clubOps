0489b461b65b83de711712dbe642695598fa4325
Sujal-Shrestha-SS <sujalstha801@gmail.com>
2026-02-28 19:50:09 +0545

    User side season navbar

---
+++ sidebar.php
@@ -0,0 +1,9 @@
+<!-- Sidebar -->
+<div class="sidebar">
+    <h2>Menu</h2>
+    <a href="season.php?id=<?php echo $season_id; ?>">Insert Fixture</a>
+    <a href="insert_players.php?id=<?php echo $season_id; ?>">Insert Players</a>
+    <a href="league_table.php?id=<?php echo $season_id; ?>">Show Club Info</a>
+    <a href="player_stats.php?id=<?php echo $season_id; ?>">Show Player Stats</a>
+    <a href="videos.php?id=<?php echo $season_id; ?>">Insert Media</a>
+</div>
