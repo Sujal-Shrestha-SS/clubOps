68d745c1ff62d79ac2e3158971e9c4745afcbe11
Sujal-Shrestha-SS <sujalstha801@gmail.com>
2026-02-28 19:53:47 +0545

    Admin page sidebar to navigate through backend tasks

---
+++ home_sidebar.php
@@ -0,0 +1,11 @@
+<!-- Sidebar -->
+<div class="home-sidebar">
+    <h2>Menu</h2>
+    <a href="club.php?id=<?php echo $season_id; ?>">Players</a>
+    <a href="club_fixtures.php?id=<?php echo $season_id; ?>">Fixtures</a>
+    <a href="club_player_statistics.php?id=<?php echo $season_id; ?>">Statistics</a>
+    <a href="club_player_performance.php?id=<?php echo $season_id; ?>">Player Performance</a>
+    <a href="club_performance.php?id=<?php echo $season_id; ?>">Club Performance</a>
+    <a href="club_player_combination.php?id=<?php echo $season_id; ?>">Player Combinations</a>
+    <a href="club_media.php?id=<?php echo $season_id; ?>">Club Media</a>
+</div>
